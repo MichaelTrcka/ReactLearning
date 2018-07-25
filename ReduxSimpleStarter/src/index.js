65df6c79984996d70d6f3db3737a47ca223a968c
michael trcka <michaeltrcka@michaels-mbp.attlocal.net>
2018-07-25 12:59:25 -0500

    finished

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAko3_-3qn6Ok5vWN-_FMiIHVIvkqXXZxY';
 
@@ -14,17 +16,38 @@ class App extends Component {
     constructor(props){
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selecetedVideo: null
+        };
 
-        YTSearch({key: API_KEY, term: 'react js'}, (videos) => {
-            this.setState({ videos })
+        this.videoSearch("react js")
+    }
+    
+    videoSearch = (search) =>{
+        YTSearch({key:API_KEY, term: search}, videos=>{
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
         })
     }
+
     render(){
+
+        const videoSearch = _.debounce( (term)=>{this.videoSearch(term)} , 300 );
+
         return (
-            <div>
-                <SearchBar />
-                <VideoList  videos={ this.state.videos}/>
+            <div className = "back-ground">
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => {
+                        this.setState({selectedVideo})
+                        console.log(selectedVideo)
+                    }}
+                    videos={ this.state.videos}
+                 />
             </div>
         )
     }
